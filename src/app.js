c25b93566f61f6a5fcf8fc4985a00401857a04a8
Jose M. Torres <jmtorresr@gmail.com>
2020-10-06 14:41:27 +0900

    Added js functionality to the app. Still not functioning

--- src/app.js
+++ src/app.js
@@ -1,40 +1,51 @@
-/* Example Code
+window.onload = () => {
+  /* Example Code
       The following is just some example code for you to play around with.
       No need to keep this---it's just some code so you don't feel too lonely.
 */
 
-// How can we use require here if it's frontend? We can thank webpack.
-const Sort = require("./Sort");
+  // How can we use require here if it's frontend? We can thank webpack.
+  const Sort = require("./Sort");
 
-// A link to our styles!
-require("./index.css");
+  // A link to our styles!
+  require("./index.css");
 
-const sort = new Sort();
-sort.sort();
+  const sort = new Sort();
+  sort.sort();
 
-function createCheesyTitle(slogan) {
-  const container = document.createElement("h1");
-  const textNode = document.createTextNode(slogan);
-  container.appendChild(textNode);
-  return container;
-}
+  const arrayButton = document.createElement("button");
+  arrayButton.className = "fig";
+  arrayButton.innerText = "Give me an Array";
+  arrayButton.addEventListener("mouseover", () => {
+    alert("define the array");
+  });
+  document.body.prepend(arrayButton);
 
-const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
-document.getElementById("title").appendChild(title);
+  function cresteArray(array) {
+    const container = document.createElement("div");
+    container.className = "box";
+    const textNode = document.createTextNode(slogan);
+    container.appendChild(textNode);
+    return container;
+  }
 
-/*
+  const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
+  document.getElementById("title").appendChild(title);
+
+  /*
     An simple example of how you can make your project a bit more
     interactive, if you would like.
 
     In our `index.html` page, we have a short form.
     Here is the code that talks to it.
   */
-function changeTitle(event) {
-  event.preventDefault();
-  // console.log('What is an event?', event);
-}
+  function changeTitle(event) {
+    event.preventDefault();
+    // console.log('What is an event?', event);
+  }
 
-const form = document.querySelector("form");
-document.addEventListener("DOMContentLoaded", () => {
-  form.onsubmit = changeTitle;
-});
+  const form = document.querySelector("form");
+  document.addEventListener("DOMContentLoaded", () => {
+    form.onsubmit = changeTitle;
+  });
+};
